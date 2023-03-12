80f29cc38031349aea0bce3a55bd8fea6bb65f3c
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-12 21:45:44 +0300

    fix...

--- source/Main.hx
+++ source/Main.hx
@@ -6,11 +6,21 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+        private var gameWidth:Int = 960;
+	private var gameHeight:Int = 640;
+
 	public function new()
 	{
                 SUtil.uncaughtErrorHandler();
 		super();
 		openfl.utils._internal.Log.level = LogLevel.WARN;
-		addChild(new FlxGame(0, 0, PlayState));
+                final stageWidth:Int = Lib.current.stage.stageWidth;
+		final stageHeight:Int = Lib.current.stage.stageHeight;
+		final ratioX:Float = stageWidth / gameWidth;
+		final ratioY:Float = stageHeight / gameHeight;
+		final zoom:Float = Math.min(ratioX, ratioY);
+		gameWidth = Math.ceil(stageWidth / zoom);
+		gameHeight = Math.ceil(stageHeight / zoom);
+		addChild(new FlxGame(gameWidth, gameHeight, PlayState, zoom));
 	}
 }
